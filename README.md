a405e4714bfff167c3a0b8971921ccff03f06776
DevkotaPratyush <pratyush.devkota@selu.edu>
2021-10-27 01:10:14 -0500

    Added a carousel banner and product directory

--- README.md
+++
@@ -1,6 +0,0 @@
-# EcommerceApplication
-Full stack e-commerce web application
-
-dependencies-
-npm install -g firebase-tools 
-npm install react-icons --save
--- shopper/src/Components/Header/Header.jsx
+++ shopper/src/Components/Header/Header.jsx
@@ -1,51 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './Header.css'
-import logo from './logo.png'
-import SearchIcon from '@material-ui/icons/Search';
-import {MdSearch} from 'react-icons/md'
-import {AiOutlineShoppingCart} from 'react-icons/ai'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Header.css";
+import logo from "./logo.png";
+// import SearchIcon from '@material-ui/icons/Search';
+import { MdSearch } from "react-icons/md";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 function Header() {
-    return (
-        <div className='header'>
-            <img 
-            className='header_logo'
-            src={logo}/>
-            
-            <div className='header_search'>
-                
-                <input className='header_searchInput' type='text'/>
-                <MdSearch className='SearchIcon'/>
-            </div>
-            
+  return (
+    <div className="header">
+      <img className="header_logo" src={logo} />
 
-            <div className='header_nav'>
+      <div className="header_search">
+        <input className="header_searchInput" type="text" />
+        <MdSearch className="SearchIcon" />
+      </div>
 
-                <div className='header_option'>
-                    <span className= 'header_optionLineOne'> Hello Guest</span>
-                    <span className= 'header_optionLineTwo'> Sign In</span>
-                </div>
-
-                <div className='header_option'>
-                <span className= 'header_optionLineOne'> Order</span>
-                <span className= 'header_optionLineTwo'> History</span>
-                </div>
-                
-                <div className='header_option'>
-                <span className= 'header_optionLineOne'> Your</span>
-                <span className= 'header_optionLineTwo'> Membership</span> 
-                
-                </div>
-
-                <div className='header_option_Basket'>
-                <AiOutlineShoppingCart/>
-                <span className="header_optionLineTwo
-                header_basketCount">0</span>
-                </div>
-            </div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionLineOne"> Hello Guest</span>
+          <span className="header_optionLineTwo"> Sign In</span>
         </div>
-    
-    )
+
+        <div className="header_option">
+          <span className="header_optionLineOne"> Order</span>
+          <span className="header_optionLineTwo"> History</span>
+        </div>
+
+        <div className="header_option">
+          <span className="header_optionLineOne"> Your</span>
+          <span className="header_optionLineTwo"> Membership</span>
+        </div>
+
+        <div className="header_option_Basket">
+          <AiOutlineShoppingCart />
+          <span
+            className="header_optionLineTwo
+                header_basketCount"
+          >
+            0
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
